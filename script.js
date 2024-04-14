c3095da6f26a5a4345b2f95b7251ca0bb568dac8
DuriKang123 <167036427+DuriKang123@users.noreply.github.com>
2024-04-14 19:29:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.main-content').style.display = 'block'; // Make the main content visible
+    }, 4000); // This should match the duration of the logo's fadeOut
+});
